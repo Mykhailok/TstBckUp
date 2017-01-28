2abdbae7e53b3e78e2c6c16594b3cf781c7a0ada
Mykhailok <kosinskiy.mihael@gmail.com>
2017-01-28 15:36:55 +0200

    check two strings

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        Permutation check = new Permutation();
+
+        System.out.println("Two strings are Annotations: " + check.permutation("sadasd", "sadasdd"));
+
+    }
+}
